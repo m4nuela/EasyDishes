8626c146964a3e41c8d4a160fa707b6838852a17
nogn <Nicolas94@Nicolass-MBP.westell.com>
2016-03-12 20:00:44 -0600

    nada

--- EasyDishesProject/CreateAccountViewController.swift
+++ EasyDishesProject/CreateAccountViewController.swift
@@ -119,28 +119,12 @@ class CreateAccountViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        txtEmail.placeholder = "Email"
-        txtUsername.placeholder = "Username"
-        txtPassword.placeholder = "Password"
-        txtPasswordAgain.placeholder = "Password Again"
-        // Do any additional setup after loading the view.
+      
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
 }
